0466f404ea9b4604439a8696c6a4fad3e6bf1944
Joshua C. Sadsad <joshsad2@gmail.com>
2022-01-12 11:27:08 -0800

    done with budgetContxt

---
+++ src/contexts/BudgetsContext.js
@@ -0,0 +1,59 @@
+import React, { useContext, useState } from 'react'
+import { v4 as uuidV4 } from 'uuid'
+
+const BudgetsContext = React.createContext()
+
+export function useBudgets() {
+  return useContext(BudgetsContext)
+}
+
+export const BudgetsProvider = ({ children }) => {
+  const [budgets, setBudgets] = useState([])
+  const [expenses, setExpenses] = useState([])
+
+  const getBudgetExpenses = (budgetId) => {
+    return expenses.filter((expense) => expense.budgetId === budgetId)
+  }
+
+  const addExpense = ({ description, amount, budgetID }) => {
+    setExpense((prevExpenses) => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetID }]
+    })
+  }
+
+  const addBudget = ({ name, max }) => {
+    setBudgets((prevBudgets) => {
+      if (prevBudgets.find((budget) => budget.name === name)) return prevBudgets
+
+      return [...prevBudgets, { id: uuidV4(), name, max }]
+    })
+  }
+
+  const deleteBudget = ({ id }) => {
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter(budget > budget.id !== id)
+    })
+  }
+
+  const deleteExpense = ({ id }) => {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter(expense > expense.id !== id)
+    })
+  }
+
+  return (
+    <BudgetsContext.Provider
+      value={
+        (budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense)
+      }
+    >
+      {children}
+    </BudgetsContext.Provider>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App'
+import { BudgetsProvider } from './contexts/BudgetsContext' // making everything here available in our App.
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BudgetsProvider>
+      <App />
+    </BudgetsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
