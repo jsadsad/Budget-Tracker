a50a8afb58dbedee4e0708cf34ebb3cc9cea1540
Joshua C. Sadsad <joshsad2@gmail.com>
2022-01-13 13:46:33 -0800

    modal successfully rendering with state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { useState } from 'react'
 import { Button, Stack } from 'react-bootstrap'
 import Container from 'react-bootstrap/Container'
 import AddBudgetModal from './components/AddBudgetModal'
 import BudgetCard from './components/BudgetCard'
 
 const App = () => {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
   return (
     <>
       <Container className="my-4">
         <Stack direction="horizontal" gap="2" className="mb-4">
           <h1 className="me-auto">Budgets</h1>
-          <Button variant="primary">Add Budget</Button>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
           <Button variant="outline-primary">Add Expense</Button>
         </Stack>
         <div
@@ -28,7 +32,10 @@ const App = () => {
           ></BudgetCard>
         </div>
       </Container>
-      <AddBudgetModal show="true" />
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
     </>
   )
 }
