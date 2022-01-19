87dd503a6dc75c7fcd50dadeb8970bc828f3f960
Joshua C. Sadsad <joshsad2@gmail.com>
2022-01-19 12:56:28 -0800

    properly styled"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,52 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
   return (
     <Card.Footer className="fixed-bottom text-muted text-center">
-      2 days ago
+      <Container>
+        <Row>
+          <Col>
+            <a
+              href="https://github.com/jsadsad/react-budget-app"
+              target="_blank"
+              style={{
+                textDecoration: 'none',
+                fontSize: '1rem',
+                color: '#58987A',
+              }}
+            >
+              Github
+            </a>
+          </Col>
+          <Col>
+            <a
+              href="https://jsadsad.io/"
+              target="_blank"
+              style={{
+                textDecoration: 'none',
+                fontSize: '1rem',
+                color: '#58987A',
+              }}
+            >
+              @jsadsad
+            </a>
+          </Col>
+          <Col>
+            <a
+              href="https://www.linkedin.com/in/joshsad2/"
+              target="_blank"
+              style={{
+                textDecoration: 'none',
+                fontSize: '1rem',
+                color: '#58987A',
+              }}
+            >
+              LinkedIn
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </Card.Footer>
   )
 }
